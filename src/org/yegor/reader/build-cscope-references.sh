90ae69fa44910f83ff869a6a588dd209a6dfb316
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2012-09-24 20:50:12 +0400

    Fixed cscope java source database generator

--- src/org/yegor/reader/build-cscope-references.sh
+++ src/org/yegor/reader/build-cscope-references.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cscope -b -i <(find src /home/keeper/source/android/libcore/luni/src/main/java /home/keeper/source/android/frameworks/base -type f -name '*.java')
+cscope -b -i <(find . /home/keeper/source/android/libcore/luni/src/main/java /home/keeper/source/android/frameworks/base -type f -name '*.java')
