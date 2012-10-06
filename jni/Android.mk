8c4b18667e8300787cec5f995227e750a4cff64f
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2012-10-06 16:25:41 +0400

    Enabled gnu++0x support

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,5 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_LDLIBS += -llog
 LOCAL_MODULE := preview-processor
 LOCAL_SRC_FILES := preview-processor.cpp
+LOCAL_CPPFLAGS += -std=gnu++0x
 
 include $(BUILD_SHARED_LIBRARY)
